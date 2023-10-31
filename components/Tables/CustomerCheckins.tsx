d819862675d4537eb74dcda9e685fa367655c73b
vikram <saran3vikram@gmail.com>
2023-10-31 10:20:13 +0530

    imported react from react in customercheckin

--- components/Tables/CustomerCheckins.tsx
+++ components/Tables/CustomerCheckins.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { useDashboard } from "@/contexts/DashboardContext"
 import api from "@/utils/api"
 
